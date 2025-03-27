9531bf98afb284e602bc280760a3ecc84d3182da
Matt42200 <mranderson4@students.unwsp.edu>
2025-03-27 09:33:08 -0500

    Add files via upload

---
+++ Item Counter Program.py
@@ -0,0 +1,9 @@
+﻿try:
+    with open('names.txt', 'r') as file:
+        names = file.readlines()
+        
+        num_names = len(names)
+        
+        print(f"The number of names in the file is: {num_names}")
+except FileNotFoundError:
+    print("The file 'names.txt' was not found.")
